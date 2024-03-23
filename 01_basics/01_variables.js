1a7deb3104dac741b86dda1d9f6778a66aeaeada
Deepanshu Sinha <103493361+deep2334@users.noreply.github.com>
2024-03-23 05:34:05 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+let accountId = "Deepanshu@2004"
+const accountNumber= 567896
+var  accountPassword= "India@404"
+accountCity= "Patna"
+let accountholder=""
+console.table([accountId,accountNumber,accountPassword,accountCity,accountholder])
+
+/*
+do not use var because of block scope
+
+*/
+
+
