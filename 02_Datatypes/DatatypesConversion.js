8a0a5a786c6501fd8e6068b809065670f1332930
Deepanshu Sinha <103493361+deep2334@users.noreply.github.com>
2024-03-23 06:47:59 +0000

    conversion

---
+++ 02_Datatypes/DatatypesConversion.js
@@ -0,0 +1,10 @@
+let score = "33abc"
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); /* in javascript there is no strict check */
+/* this giving value of NaN */
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
